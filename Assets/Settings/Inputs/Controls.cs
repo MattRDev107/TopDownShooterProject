1c9f20d6a9111206dd49176ef0d62695cc9b6f59
Matthew R <mattsdev107@gmail.com>
2020-12-30 23:17:49 -0600

    Add More Key bind for weapon select

--- Assets/Settings/Inputs/Controls.cs
+++ Assets/Settings/Inputs/Controls.cs
@@ -41,6 +41,38 @@ public class @Controls : IInputActionCollection, IDisposable
                     ""expectedControlType"": ""Button"",
                     ""processors"": """",
                     ""interactions"": """"
+                },
+                {
+                    ""name"": ""PrimaryWeapon"",
+                    ""type"": ""Button"",
+                    ""id"": ""71eca9ba-d34c-4baa-8103-20044c09ddc3"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""SecondaryWeapon"",
+                    ""type"": ""Button"",
+                    ""id"": ""7be9c0df-6d86-4986-ba49-6a6c08f6c6c7"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""EquipmentSlot"",
+                    ""type"": ""Button"",
+                    ""id"": ""57b3ff92-35ad-4bab-b8b5-23190c420cb7"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""GrenadeThrow"",
+                    ""type"": ""Button"",
+                    ""id"": ""6133ab22-bf09-4138-b709-16217f48b074"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
                 }
             ],
             ""bindings"": [
@@ -175,6 +207,50 @@ public class @Controls : IInputActionCollection, IDisposable
                     ""action"": ""MousePostion"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""98b2f643-4217-4bdd-8491-6753ed7a37c4"",
+                    ""path"": ""<Keyboard>/1"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""KeyboardAndMouse"",
+                    ""action"": ""PrimaryWeapon"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""a6367c36-a242-4b6e-af47-8c0eaa8af46d"",
+                    ""path"": ""<Keyboard>/2"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""KeyboardAndMouse"",
+                    ""action"": ""SecondaryWeapon"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""609b6206-3e3e-4926-807c-ec35e2f04602"",
+                    ""path"": ""<Keyboard>/3"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""KeyboardAndMouse"",
+                    ""action"": ""EquipmentSlot"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""f096ed49-61db-46c6-8828-69d52894a54e"",
+                    ""path"": ""<Keyboard>/g"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""KeyboardAndMouse"",
+                    ""action"": ""GrenadeThrow"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
                 }
             ]
         }
@@ -203,6 +279,10 @@ public class @Controls : IInputActionCollection, IDisposable
         m_Player_movement = m_Player.FindAction("movement", throwIfNotFound: true);
         m_Player_MousePostion = m_Player.FindAction("MousePostion", throwIfNotFound: true);
         m_Player_Shoot = m_Player.FindAction("Shoot", throwIfNotFound: true);
+        m_Player_PrimaryWeapon = m_Player.FindAction("PrimaryWeapon", throwIfNotFound: true);
+        m_Player_SecondaryWeapon = m_Player.FindAction("SecondaryWeapon", throwIfNotFound: true);
+        m_Player_EquipmentSlot = m_Player.FindAction("EquipmentSlot", throwIfNotFound: true);
+        m_Player_GrenadeThrow = m_Player.FindAction("GrenadeThrow", throwIfNotFound: true);
     }
 
     public void Dispose()
@@ -255,6 +335,10 @@ public class @Controls : IInputActionCollection, IDisposable
     private readonly InputAction m_Player_movement;
     private readonly InputAction m_Player_MousePostion;
     private readonly InputAction m_Player_Shoot;
+    private readonly InputAction m_Player_PrimaryWeapon;
+    private readonly InputAction m_Player_SecondaryWeapon;
+    private readonly InputAction m_Player_EquipmentSlot;
+    private readonly InputAction m_Player_GrenadeThrow;
     public struct PlayerActions
     {
         private @Controls m_Wrapper;
@@ -262,6 +346,10 @@ public class @Controls : IInputActionCollection, IDisposable
         public InputAction @movement => m_Wrapper.m_Player_movement;
         public InputAction @MousePostion => m_Wrapper.m_Player_MousePostion;
         public InputAction @Shoot => m_Wrapper.m_Player_Shoot;
+        public InputAction @PrimaryWeapon => m_Wrapper.m_Player_PrimaryWeapon;
+        public InputAction @SecondaryWeapon => m_Wrapper.m_Player_SecondaryWeapon;
+        public InputAction @EquipmentSlot => m_Wrapper.m_Player_EquipmentSlot;
+        public InputAction @GrenadeThrow => m_Wrapper.m_Player_GrenadeThrow;
         public InputActionMap Get() { return m_Wrapper.m_Player; }
         public void Enable() { Get().Enable(); }
         public void Disable() { Get().Disable(); }
@@ -280,6 +368,18 @@ public class @Controls : IInputActionCollection, IDisposable
                 @Shoot.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnShoot;
                 @Shoot.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnShoot;
                 @Shoot.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnShoot;
+                @PrimaryWeapon.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnPrimaryWeapon;
+                @PrimaryWeapon.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnPrimaryWeapon;
+                @PrimaryWeapon.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnPrimaryWeapon;
+                @SecondaryWeapon.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnSecondaryWeapon;
+                @SecondaryWeapon.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnSecondaryWeapon;
+                @SecondaryWeapon.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnSecondaryWeapon;
+                @EquipmentSlot.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnEquipmentSlot;
+                @EquipmentSlot.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnEquipmentSlot;
+                @EquipmentSlot.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnEquipmentSlot;
+                @GrenadeThrow.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnGrenadeThrow;
+                @GrenadeThrow.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnGrenadeThrow;
+                @GrenadeThrow.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnGrenadeThrow;
             }
             m_Wrapper.m_PlayerActionsCallbackInterface = instance;
             if (instance != null)
@@ -293,6 +393,18 @@ public class @Controls : IInputActionCollection, IDisposable
                 @Shoot.started += instance.OnShoot;
                 @Shoot.performed += instance.OnShoot;
                 @Shoot.canceled += instance.OnShoot;
+                @PrimaryWeapon.started += instance.OnPrimaryWeapon;
+                @PrimaryWeapon.performed += instance.OnPrimaryWeapon;
+                @PrimaryWeapon.canceled += instance.OnPrimaryWeapon;
+                @SecondaryWeapon.started += instance.OnSecondaryWeapon;
+                @SecondaryWeapon.performed += instance.OnSecondaryWeapon;
+                @SecondaryWeapon.canceled += instance.OnSecondaryWeapon;
+                @EquipmentSlot.started += instance.OnEquipmentSlot;
+                @EquipmentSlot.performed += instance.OnEquipmentSlot;
+                @EquipmentSlot.canceled += instance.OnEquipmentSlot;
+                @GrenadeThrow.started += instance.OnGrenadeThrow;
+                @GrenadeThrow.performed += instance.OnGrenadeThrow;
+                @GrenadeThrow.canceled += instance.OnGrenadeThrow;
             }
         }
     }
@@ -311,5 +423,9 @@ public class @Controls : IInputActionCollection, IDisposable
         void OnMovement(InputAction.CallbackContext context);
         void OnMousePostion(InputAction.CallbackContext context);
         void OnShoot(InputAction.CallbackContext context);
+        void OnPrimaryWeapon(InputAction.CallbackContext context);
+        void OnSecondaryWeapon(InputAction.CallbackContext context);
+        void OnEquipmentSlot(InputAction.CallbackContext context);
+        void OnGrenadeThrow(InputAction.CallbackContext context);
     }
 }
