29eae72502439750f74e9996288153db0cc4d2ae
Matthew R <mattsdev107@gmail.com>
2020-12-31 01:19:47 -0600

    Add the ability to switch between items
    weapons and equipment

--- Assets/Scripts/Player/InputController.cs
+++ Assets/Scripts/Player/InputController.cs
@@ -9,10 +9,16 @@ namespace MoonlanderCode.TDS.Player {
 	public class InputController : MonoBehaviour {
 
 		public bool IsShooting { get => isShooting; }
+		public bool SwitchToPrimary { get => switchToPrimary; }
+		public bool SwitchToSecondary { get => switchToSecondary; }
+		public bool SwitchToEquipment { get => switchToEquipment; }
 		public Vector2 MovementDirection { get => movementDirection; }
 		public Vector2 MouseScreenPostion { get => mousePostion; }
 
 		private bool isShooting = false;
+		private bool switchToPrimary = false;
+		private bool switchToSecondary = false;
+		private bool switchToEquipment = false;
 		private Vector2 movementDirection;
 		private Vector2 mousePostion;
 
@@ -28,9 +34,20 @@ namespace MoonlanderCode.TDS.Player {
 			playerCon = GameObject.FindObjectOfType<PlayerController>();
 
 			input.Player.movement.performed += ctx => OnMovement(ctx);
+
 			input.Player.MousePostion.performed += ctx => OnMousePostion(ctx);
+
 			input.Player.Shoot.started += ctx => isShooting = true;
 			input.Player.Shoot.canceled += ctx => isShooting = false;
+
+			input.Player.PrimaryWeapon.started += ctx => switchToPrimary = true;
+			input.Player.PrimaryWeapon.canceled += ctx => switchToPrimary = false;
+
+			input.Player.SecondaryWeapon.started += ctx => switchToSecondary = true;
+			input.Player.SecondaryWeapon.canceled += ctx => switchToSecondary = false;
+
+			input.Player.EquipmentSlot.started += ctx => switchToEquipment = true;
+			input.Player.EquipmentSlot.canceled += ctx => switchToEquipment = false;
 		}
 
 		private void OnMousePostion(InputAction.CallbackContext ctx) {
