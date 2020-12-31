1e95d7c453734b37d6bc57c48f596cb7da71252a
Matthew R <mattsdev107@gmail.com>
2020-12-30 23:17:02 -0600

    Move CameraController to Player
    Move CameraController to player namespace

---
+++ Assets/Scripts/Player/CameraController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MoonlanderCode.TDS.Player;
+
+namespace MoonlanderCode.TDS.Player {
+
+	public class CameraController : MonoBehaviour {
+
+		[SerializeField] private Transform parent;
+		[SerializeField] private float radius = 2f;
+		[SerializeField] private float smoothTime = 0.5f;
+
+		private float distance;
+		private InputController input;
+
+		private void Start() {
+			input = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>().inputController;
+			parent = GameObject.FindGameObjectWithTag("Player").transform;
+		}
+
+		private void FixedUpdate() {
+			FollowBetweenPlayerCamera();
+		}
+
+		private void FollowBetweenPlayerCamera() {
+			Vector2 mouseWorldPostion = Camera.main.ScreenToWorldPoint(input.MouseScreenPostion);
+			Vector3 mouseOffset = (Vector3)mouseWorldPostion - parent.position;
+			Vector3 target = new Vector3(mouseOffset.x / 2.0f + parent.position.x, mouseOffset.y / 2.0f + parent.position.y, transform.position.z);
+
+			distance = Vector2.Distance((Vector2)target, (Vector2)parent.position);
+
+			if (distance >= radius) {
+				Vector2 norm = mouseOffset.normalized;
+				target = new Vector3(norm.x * radius + parent.position.x, norm.y * radius + parent.position.y, transform.position.z);
+				transform.position = Vector3.Lerp(transform.position, target, smoothTime);
+			}
+			else {
+				transform.position = Vector3.Lerp(transform.position, target, smoothTime);
+			}
+		}
+	}
+}
