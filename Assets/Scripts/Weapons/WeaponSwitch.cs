f6790d598bb98c09b9f525dd5b6addfb727c10af
Matthew R <mattsdev107@gmail.com>
2020-12-30 23:30:27 -0600

    Start WeaponSwitcher

---
+++ Assets/Scripts/Weapons/WeaponSwitch.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MoonlanderCode.TDS.Weapon {
+
+	public class WeaponSwitch : MonoBehaviour {
+
+		private WeaponSelection WeaponSelected;
+
+		enum WeaponSelection { primary, secondary }
+
+	}
+}
